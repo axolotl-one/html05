bc5ffb011aea45e06521bf930a01a44b8c8a0fbf
AXELzip <axellp0001@gmail.com>
2025-11-03 23:58:12 -0600

    :construction: V3.0.3 Renderizado de Tabla y Manejo de sus dimensiones

--- pages/IOMinimizaciones/tabla.js
+++ pages/IOMinimizaciones/tabla.js
@@ -1,14 +1,81 @@
 const table = document.getElementById('table')
 const nColumns = document.getElementById('n-columns')
 const nRows = document.getElementById('n-rows')
-let data = [
-  ['ORIGEN/DESTINO','DESTINO 1', 'DESTINO 2', 'OFERTA'],
-  ['ORIGEN 1', 0, 0, 0],
-  ['ORIGEN 2', 0, 0, 0],
-  ['DEMANDA', 0, 0, 0],
-]
+let costos = [[0,0],[0,0]]
+let unidades = [[0,0],[0,0]]
+const ofertas = [0,0]
+const demandas = [0,0]
 
-const updateData = () => {
+const renderTable = () => {
+  table.textContent = '';
+  const c = parseInt(nColumns.textContent);
+  const r = parseInt(nRows.textContent);
+  for(let i = 0; i<c+2; i++) {
+    const dest = document.createElement('span');
+    i===0 ? dest.appendChild(generarCelda('o-d','Origen\\Destino','text'))
+    : i===c+1 ? dest.appendChild(generarCelda('oferta','Oferta (Recursos)','text'))
+    : dest.appendChild(generarCelda('dest'+i, 'Destino ' + i, 'text'))
+    table.appendChild(dest);
+  }
+  for(let i = 0; i<r; i++){
+    const row = document.createElement('div');
+    const orig = document.createElement('span');
+    orig.appendChild(generarCelda('org'+(i+1),'Origen ' + (i+1),'text'))
+    row.appendChild(orig);
+    for(let j = 0; j<c; j++) {
+      const cell = document.createElement('span');
+      cell.appendChild(generarCelda('c-' + i + '-' + j,costos[i][j]));
+      cell.appendChild(generarCelda('u-' + i + '-' + j,unidades[i][j]));
+      row.appendChild(cell);
+    }
+    const oferta = document.createElement('span')
+    oferta.appendChild(generarCelda('of'+i,ofertas[i]))
+    row.appendChild(oferta)
+    table.appendChild(row)
+  }
+  for(let i = 0; i<c+2; i++){
+    const demanda = document.createElement('span')
+    i===0 ? demanda.appendChild(generarCelda('demanda','Demanda','text'))
+    : i===c+1 ? demanda.appendChild(generarCelda('total',0))
+    : demanda.appendChild(generarCelda('d'+i,demandas[i-1]))
+    table.appendChild(demanda)
+  }
+}
+
+const generarCelda = (id,value,type='number') => {
+  const cell = document.createElement('input');
+  cell.inputMode = type==='number'?'numeric':'';
+  cell.type = type==='number'?'number':'text';
+  cell.id = id;
+  cell.value = value;
+  return cell;
+}
+
+const handleLength = (element, isRows, isPlus) => {
+  if(parseInt(element.textContent)<=2 && !isPlus) { return; }
+  element.textContent = isPlus ? parseInt(element.textContent)+1 : parseInt(element.textContent)-1;
+  isRows ? updateRowsData(costos,isPlus) & updateRowsData(unidades,isPlus) // Si son filas, las actualiza
+        : updateColumnsData(costos,isPlus) & updateColumnsData(unidades,isPlus); // sino actualiza las columnas
+  isRows ? isPlus ? ofertas.push(0) : ofertas.pop() //
+  : isPlus ? demandas.push(0) : ofertas.pop();
+  renderTable();
+}
+
+const updateColumnsData = (data, plus) => {
+  console.log(data)
+  if(plus) { for(let i = 0; i<data.length; i++) {data[i].push(0)} ; return }
+  for(let i = 0; i<data.length; i++) { data[i].pop }
+  console.log(data)
+}
+
+const updateRowsData = (data, plus) => {
+  console.log(data)
+  plus ? data.push(data[0].map(()=>0)) : data.pop()
+  console.log(data)
+}
+
+/*
+const updateDatax = (data) => {
   const newData = []
   for(let i = 0; i<parseInt(nRows.textContent);i++){
     newData[i] = []
@@ -21,13 +88,11 @@ const updateData = () => {
   console.log(data);
   table.textContent=data;
 }
+  */
 
-document.getElementById('rest-column').addEventListener('click', ()=>{nColumns.textContent = parseInt(nColumns.textContent)-1;updateData()});
-document.getElementById('plus-column').addEventListener('click', ()=>{nColumns.textContent = parseInt(nColumns.textContent)+1;updateData()});
-document.getElementById('rest-row').addEventListener('click', ()=>{nRows.textContent = parseInt(nRows.textContent)-1;updateData()});
-document.getElementById('plus-row').addEventListener('click', ()=>{nRows.textContent = parseInt(nRows.textContent)+1;updateData()});
+document.getElementById('rest-column').addEventListener('click', ()=>{handleLength(nColumns,false,false)});
+document.getElementById('plus-column').addEventListener('click', ()=>{handleLength(nColumns,false,true)});
+document.getElementById('rest-row').addEventListener('click', ()=>{handleLength(nRows,true,false)});
+document.getElementById('plus-row').addEventListener('click', ()=>{handleLength(nRows,true,true)});
 
-
-const tabla = document.createElement('section')
-const valor = document.getElementById('inputValor')
-
+renderTable();
