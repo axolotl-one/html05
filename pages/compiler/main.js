654a90ad4e1a1198f2cdc4ed13b2f47257a31db7
AXELzip <axellp0001@gmail.com>
2026-03-14 23:58:43 -0600

    :construction: V3.2.2 Refactorización de V3.2.1 y pruebas de AST

--- pages/compiler/main.js
+++ pages/compiler/main.js
@@ -5,8 +5,10 @@ editor.value = "public void main() {\n   Console.WriteLine(\"Hola Mundo...!\");\
 window.compilar = function() {
   if(!editor.value) return;
   consola.value = "Generando Lista de Tokens:\n"
-  for(let token of generarTokens(editor.value))
-    consola.value +=  token.tipo + ": " + token.valor + "\n";
+  const tokens = generarTokens(editor.value);
+  if(!tokens) return;
+  consola.value += "Generando AST";
+  console.log(armarAST(new NodoToken("main"), tokens, 0, ));  
 }
 
 const tiposToken = {
@@ -20,18 +22,12 @@ const tiposToken = {
 };
 
 function generarTokens(input) {
+  const tokens = [];
   let pin = 0;
-  let tokens = [];
-
+  
   // Definir las reglas de los tokens
   const reglas = [
-    { tipo: tiposToken.Delimitador, regex: /^\(/ },
-    { tipo: tiposToken.Delimitador, regex: /^\)/ },
-    { tipo: tiposToken.Delimitador, regex: /^\{/ },
-    { tipo: tiposToken.Delimitador, regex: /^\}/ },
-    { tipo: tiposToken.Delimitador, regex: /^\;/ },
-    { tipo: tiposToken.Delimitador, regex: /^\./ },
-    { tipo: tiposToken.Delimitador, regex: /^\,/ },
+    { tipo: tiposToken.Delimitador, regex: /^[(){};,\[\]]/ },
     { tipo: tiposToken.Numero, regex: /^[0-9]+/ },
     { tipo: tiposToken.Ignorable, regex: /^(\/\*)([^\/\*]*)(\*\/)/}, //Comentario en bloque
     { tipo: tiposToken.Ignorable, regex: /^(\/\/)([^\/\*]*)\n/}, //Comentario en linea
@@ -44,34 +40,55 @@ function generarTokens(input) {
     { tipo: tiposToken.Ignorable, regex: /^\n+/ }, // Saltos de Linea
     // ^(inicia con)([^cualquiera que no sea]*)(termina con)
   ];
-
+  
   while (pin < input.length) {
     let char = input[pin];
     let matched = false;
-
+    
     for (let regla of reglas) {
       let match = regla.regex.exec(input.slice(pin));
       
       if (match) {
-        let value = match[0];
+        let token = match[0];
         // No añadir espacios en blanco a la lista final de tokens
         if (regla.tipo !== tiposToken.Ignorable) {
-          tokens.push({ tipo: regla.tipo, valor: value });
+          tokens.push({ tipo: regla.tipo, valor: token });
+          consola.value +=  regla.tipo + ": " + token + "\n";
         }
-        pin += value.length;
+        pin += token.length;
         matched = true;
         break;
       }
     }
 
     if (!matched) {
-      tokens.push({tipo: "Error: Caracter Desconocido", valor: char + "\nEjecución Abortada."});
-      return tokens;
+      consola.value += "Error: Caracter Desconocido: \"" + char + "\".\n";
+      consola.value += "Ejecución Abortada.";
+      return;
       //pin++; continue;
     }
   }
 
   return tokens;
+}
+
+class NodoToken{
+  constructor(raiz) { this.raiz = raiz; this.nodo1 = null; this.nodo2 = null }
+
+
+}
+
+function armarAST(nodo, tokens, pin) {
+  
+  if(pin >= tokens.length) return;
+
+  if(tokens[pin].tipo === tiposToken.Id && tokens[pin + 1] !== tiposToken.Operador) return nodo;
+  if(tokens[pin].tipo === tiposToken.Delimitador && tokens[pin + 1].valor === ";") return nodo;
+
+  if(tokens[pin].tipo === tiposToken.Reservado && tokens[pin+1].tipo === tiposToken.Id)
+    nodo.nodo1 = armarAST(new NodoToken(tokens[pin+1]), tokens, pin+1)
+
+  return nodo;
 }
 
 //compilar();
