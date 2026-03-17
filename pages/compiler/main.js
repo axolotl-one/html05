ef1bdda53506ffdef0fa7b43d8e17cdcb2e1ee94
AXELzip <axellp0001@gmail.com>
2026-03-16 23:59:29 -0600

    :hammer: V3.2.4 Refactor Generación del Árbol AST con JSON

--- pages/compiler/main.js
+++ pages/compiler/main.js
@@ -1,29 +1,42 @@
 const editor = document.getElementById("codigo-fuente");
 const consola = document.getElementById("consola");
+const mapaAst = document.getElementById("ast");
 //editor.value = "public void main() {\n   Console.WriteLine(\"Hola Mundo...!\");\n   Console.ReadKey(true);\n}"
-editor.value = "string c = \"hola\";\nstring v = c;"
+editor.value = "string c = \"hola\";\nstring v = c;";
+const Gtokens = [];
+
+window.verSection = function(selector, id){
+  document.querySelectorAll(selector).forEach(sec=>sec.style.display = 'none');
+  document.getElementById(id).style.display = 'block';
+}
 
 window.compilar = function() {
   if(!editor.value) return;
-  consola.value = "Generando Lista de Tokens:\n"
+  consola.value = "Generando Lista de Tokens:\n";
   const tokens = generarTokens(editor.value);
   if(!tokens) return;
+  Gtokens.forEach(() => { Gtokens.pop()});
+  tokens.forEach((e) => { Gtokens.push(e)});
+  Gtokens.unshift({tipo:"MAIN", valor:"origen"});
   consola.value += "Generando AST\n";
   //console.log(armarASTX(new NodoToken({tipo: "raiz", valor: "origen"}), tokens, 0 ));
-  const AST = armarAST(new NodoToken("origen"), [...tokens])
+  const AST = armarAST(new NodoToken("origen"));
   console.log(AST);
-  console.log(JSON.stringify(AST, null, 2))
-  consola.value += JSON.stringify(AST, null, 2)
+  console.log(JSON.stringify(AST, null, 2));
+  consola.value += JSON.stringify(AST, null, 2);
+  mapaAst.innerHTML = "<p>Armando Árbol AST</p>";
 }
 
-const tiposToken = {
-  Id: "Identificador",
-  Numero: "Numero",
-  Reservado: "Palabra Reservada",
-  Delimitador: "Delimitador",
-  String: "Cadena de Caracteres",
-  Operador: "Operador",
-  Ignorable: "Espacios/Saltos/Comentarios",
+const CLASETOKEN = {
+  ID: "Identificador",
+  NUMERO: "Número",
+  RESERVADO: "Palabra Reservada",
+  DECLARADOR: "Declarador de Tipo",
+  DELIMITADOR: "Delimitador",
+  BOOLEANDO: "Booleano",
+  STRING: "Cadena de Caracteres",
+  OPERADOR: "Operador",
+  IGNORADO: "Espacios/Saltos/Comentarios",
 };
 
 function generarTokens(input) {
@@ -32,17 +45,19 @@ function generarTokens(input) {
   
   // Definir las reglas de los tokens
   const reglas = [
-    { tipo: tiposToken.Delimitador, regex: /^[(){};,\[\]]/ },
-    { tipo: tiposToken.Numero, regex: /^[0-9]+/ },
-    { tipo: tiposToken.Ignorable, regex: /^(\/\*)([^\/\*]*)(\*\/)/}, //Comentario en bloque
-    { tipo: tiposToken.Ignorable, regex: /^(\/\/)([^\/\*]*)\n/}, //Comentario en linea
-    { tipo: tiposToken.Operador, regex: /^(==|>=|<=|!=|\|\||&&|\+\+|--|\+=|-=|\*=|\/=)/ },
-    { tipo: tiposToken.Operador, regex: /^[+\-*/=%!><]/ },
-    { tipo: tiposToken.String, regex: /^"([^"]*)"|^'([^']*)'/ },
-    { tipo: tiposToken.Reservado, regex: /^(int|string|void|public|private|true|false)/ },
-    { tipo: tiposToken.Id, regex: /^[a-zA-Z_$][a-zA-Z0-9_$]*/ },
-    { tipo: tiposToken.Ignorable, regex: /^\s+/ }, // Espacios en blanco
-    { tipo: tiposToken.Ignorable, regex: /^\n+/ }, // Saltos de Linea
+    { tipo: CLASETOKEN.DELIMITADOR, regex: /^[(){};,\[\]]/ },
+    { tipo: CLASETOKEN.NUMERO, regex: /^[0-9]+/ },
+    { tipo: CLASETOKEN.IGNORADO, regex: /^(\/\*)([^\/\*]*)(\*\/)/}, //Comentario en bloque
+    { tipo: CLASETOKEN.IGNORADO, regex: /^(\/\/)([^\/\*]*)\n/}, //Comentario en linea
+    { tipo: CLASETOKEN.OPERADOR, regex: /^(==|>=|<=|!=|\|\||&&|\+\+|--|\+=|-=|\*=|\/=)/ },
+    { tipo: CLASETOKEN.OPERADOR, regex: /^[+\-*/=%!><]/ },
+    { tipo: CLASETOKEN.STRING, regex: /^"([^"]*)"|^'([^']*)'/ },
+    { tipo: CLASETOKEN.DECLARADOR, regex: /^(int|float|string|void|bool|object)/ },
+    { tipo: CLASETOKEN.RESERVADO, regex: /^(if|else|while|for|return|public|private)/ },
+    { tipo: CLASETOKEN.BOOLEANDO, regex: /^(true|false)/ },
+    { tipo: CLASETOKEN.ID, regex: /^[a-zA-Z_$][a-zA-Z0-9_$]*/ },
+    { tipo: CLASETOKEN.IGNORADO, regex: /^\s+/ }, // Espacios en blanco
+    { tipo: CLASETOKEN.IGNORADO, regex: /^\n+/ }, // Saltos de Linea
     // ^(inicia con)([^cualquiera que no sea]*)(termina con)
   ];
   
@@ -56,7 +71,7 @@ function generarTokens(input) {
       if (match) {
         let token = match[0];
         // No añadir espacios en blanco a la lista final de tokens
-        if (regla.tipo !== tiposToken.Ignorable) {
+        if (regla.tipo !== CLASETOKEN.IGNORADO) {
           tokens.push({ tipo: regla.tipo, valor: token });
           consola.value +=  regla.tipo + ": " + token + "\n";
         }
@@ -85,41 +100,49 @@ class ErrorSintactico {
   constructor(error) { this.error = error }
 }
 
-function armarAST(nodo, tokens) {
-  if(nodo.raiz === ";") return nodo;
-  if(tokens.length < 1) return;
-  console.log("nodo: ", nodo.raiz)
-  console.log("token1: ", tokens[0].valor)
-  if(tokens[1]) console.log("token2: ", tokens[1].valor)
-  console.log("tokens: ", tokens)
-  console.log("length: ", tokens.length)
-  if(nodo.raiz === "origen"){
-    nodo.izquierda = armarAST(new NodoToken(tokens.shift().valor), tokens)
-    nodo.derecha = armarAST(new NodoToken("NB"), tokens)
+function armarAST() {
+  if(Gtokens.length <= 0) return;
+  const token = Gtokens.shift();
+  console.log(token);
+  const nodo = new NodoToken(token.valor);
+
+  if(token.valor === ";") return nodo;
+  console.log("nodo: ", nodo.raiz);
+  console.log("token0: ", token.valor);
+  if(Gtokens[0]) console.log("token1: ", Gtokens[0].valor); else console.log("Sin token 1");
+  if(Gtokens[1]) console.log("token2: ", Gtokens[1].valor); else console.log("Sin token 2");
+  console.log("Gtokens: ", Gtokens);
+  console.log("length: ", Gtokens.length);
+  if(token.valor === "origen"){
+    nodo.izquierda = armarAST();
+    Gtokens.unshift({tipo: "NB", valor: "NB"});
+    console.log(Gtokens);
+    nodo.derecha = armarAST();
     return nodo;
   }
 
-  if(nodo.raiz === "NB"){
-    nodo.izquierda = armarAST(new NodoToken(tokens.shift().valor), tokens)
-    if(!nodo.raiz)
-      nodo.derecha = armarAST(new NodoToken("NB"), tokens)
+  if(token.tipo === "NB"){
+    nodo.izquierda = armarAST();
+    if(Gtokens[0]){
+      Gtokens.unshift({tipo:"NB", valor: "NB"});
+      nodo.derecha = armarAST();
+    } else return nodo;
   }
 
-  if(nodo.raiz === "string"){
-    tokens[0].tipo === tiposToken.Id
-      ? nodo.izquierda = armarAST(new NodoToken(tokens.shift().valor), tokens)
-      : nodo = new ErrorSintactico("Declarador string no recibe un ID");
-    tokens[0].valor === "="
-      ? nodo.derecha = armarAST(new NodoToken(tokens.shift().valor), tokens)
-      : nodo = new ErrorSintactico("Declarador string no utiliza el operado de asignación: \"=\".")
+  if(token.tipo === CLASETOKEN.DECLARADOR){
+    if(Gtokens[0].tipo !== CLASETOKEN.ID) return new ErrorSintactico("Declarador " + token.valor + " no recibe un ID");
+    nodo.izquierda = armarAST();
+    if(Gtokens[0].valor !== '=') return new ErrorSintactico("Declarador " + token.valor + " del ID " + nodo.izquierda.raiz + " debe de estar inicializada.");
+    nodo.derecha = armarAST();
     return nodo;
   }
+
   if(nodo.raiz === "="){
-    tokens[0].valor !== ";"
-      ? nodo.izquierda = armarAST(new NodoToken(tokens.shift().valor), tokens)
+    Gtokens[0].valor !== ";"
+      ? nodo.izquierda = armarAST()
       : nodo = new ErrorSintactico("La asignación esta vacía");
-    tokens[0].tipo === tiposToken.Operador || tokens[0].valor === ";"
-      ? nodo.derecha = armarAST(new NodoToken(tokens.shift().valor), tokens)
+    Gtokens[0].tipo === CLASETOKEN.OPERADOR || Gtokens[0].valor === ";"
+      ? nodo.derecha = armarAST()
       : nodo = new ErrorSintactico("Falta punto y coma después de: " + nodo.raiz + ".")
     return nodo;
   }
@@ -143,7 +166,7 @@ function armarASTX(nodo, tokens, i) {
   }
 
   if(nodo.raiz === "string"){
-    tokens[++i].tipo === tiposToken.Id
+    tokens[++i].tipo === CLASETOKEN.ID
       ? nodo.izquierda = armarAST(new NodoToken(tokens[i].valor), tokens, i)
       : nodo = new ErrorSintactico("Declarador string no recibe un ID");
     tokens[++i].valor === "="
@@ -155,15 +178,15 @@ function armarASTX(nodo, tokens, i) {
     tokens[++i].valor !== ";"
       ? nodo.izquierda = armarAST(new NodoToken(tokens[i].valor), tokens, i)
       : nodo = new ErrorSintactico("La asignación esta vacía");
-    tokens[++i].tipo === tiposToken.Operador || tokens[i].valor === ";"
+    tokens[++i].tipo === CLASETOKEN.OPERADOR || tokens[i].valor === ";"
       ? nodo.derecha = armarAST(new NodoToken(tokens[i].valor), tokens, i)
       : nodo = new ErrorSintactico("Falta punto y coma después de: " + nodo.raiz + ".")
     return nodo;
   }
-  if(tokens[i].tipo === tiposToken.Id){
+  if(tokens[i].tipo === CLASETOKEN.ID){
     return nodo
   }
-  if(tokens[i].tipo === tiposToken.String){
+  if(tokens[i].tipo === CLASETOKEN.STRING){
     return nodo
   }
   if(nodo.izquierda instanceof ErrorSintactico) return nodo.izquierda;
